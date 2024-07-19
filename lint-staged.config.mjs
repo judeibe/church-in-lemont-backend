bfa16eaad55e1e79854d13902d8c13ff073995ce
Jude Ibe <jude.ibe@icloud.com>
2024-07-19 10:12:51 -0500

    ci: check all files with lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,12 +1,7 @@
-import path from 'path';
-
-const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`;
+const buildEslintCommand = (filenames) => `eslint --fix ${filenames.join(' ')}`;
 
 const config = {
-  '{src,apps,libs,test}/**/*.ts': [buildEslintCommand],
+  '*.{ts,mjs,js}': [buildEslintCommand],
 };
 
 export default config;
