29da2f4205726a55717598d4fe100b3ac90cad8f
Jude Ibe <jude.ibe@icloud.com>
2024-07-19 22:29:15 -0500

    ci: fix coverage path

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,17 @@
-import { pathsToModuleNameMapper, JestConfigWithTsJest } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
+import { JestConfigWithTsJest, pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
 
 const jestConfig: JestConfigWithTsJest = {
-  preset: 'ts-jest',
-  moduleDirectories: ['node_modules', '<rootDir>'],
+  preset: "ts-jest",
+  moduleDirectories: ["node_modules", "<rootDir>"],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: './',
-  testRegex: '.*\\.spec\\.ts$',
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node',
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "./",
+  testRegex: ".*\\.spec\\.ts$",
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "./coverage",
+  testEnvironment: "node",
 };
 
 export default jestConfig;
